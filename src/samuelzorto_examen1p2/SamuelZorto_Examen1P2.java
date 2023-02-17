f4181b0810b61f83bb86d017757a864b8530feb3
szorto <samuelzorto@gmail.com>
2023-02-17 13:17:41 -0600

    first comm

---
+++ src/samuelzorto_examen1p2/SamuelZorto_Examen1P2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package samuelzorto_examen1p2;
+
+/**
+ *
+ * @author szorto
+ */
+public class SamuelZorto_Examen1P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
